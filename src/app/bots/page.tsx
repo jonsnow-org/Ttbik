a85874eb32585f83ce1ce9451f54de9e85a6fb96
Brnardtsho <133473126+Brnardtsho@users.noreply.github.com>
2026-09-26 18:01:46 +0300

    G201: full /bots page restore + block-user FAQ

--- src/app/bots/page.tsx
+++ src/app/bots/page.tsx
@@ -1,10 +1,1 @@
-import type { Metadata } from "next";
-import Link from "next/link";
-import { isOwnerServer } from "@/lib/isOwner";
-import AdSlot from "@/components/AdSlot";
-import BotsDeployForm from "./BotsDeployForm";
-import { LIVE_BOTS } from "@/lib/liveBots";
-import { SITE_URL } from "@/lib/siteUrl";
-
-const SITE = SITE_URL;
-const PATH = "/bots";
+PLACEHOLDER
